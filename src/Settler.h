fd0598f8b57d8f8a81056a3c15aee30ac325e0e9
Lawri <lauri.s.lapplainen@gmail.com>
2016-11-18 22:44:01 +0200

    Basic methods & attributes

--- src/Settler.h
+++ src/Settler.h
@@ -1,4 +1,17 @@
 #pragma once
 class Settler {
 
+public:
+	Settler() : {}
+	int get_x_position const;
+	int get_y_position const;
+	void setTask(const Task t);
+	std::string getInventory;
+	
+private:
+	int x_pos;
+	int y_pos;
+	Task currentTask;
+	std::string inventory;
+	
 };
