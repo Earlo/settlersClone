4e8f8e3ed410090b53c28b5ac0bd76d3ed963d73
Earlo <theearlord@gmail.com>
2016-11-20 02:09:41 +0200

    basic terrain generation, example of terrain rendering (ugly)

--- src/mapGen/Map.h
+++ src/mapGen/Map.h
@@ -1,16 +1,48 @@
+#include <random>
+#include <time.h>
 #include "Tile.h"
+
+#include "perlin/PerlinNoise.h"
+
+
+std::default_random_engine generator(time(NULL));
+std::uniform_int_distribution<int> distribution(1000000,9999999);
 
 class Map{
 private:
 	std::vector<std::vector<Tile>> terrain;
+	//TODO make some sense in the way w and h are defined
+	//unsigned int _w;
+	//unsigned int _h;
 public:
-	Map( unsigned int x, unsigned int y ){
-	    std::cout << "doing a map of size " << x << "," << y << std::endl;
 
-		for (unsigned int i = 0; i < x; i++){
+	unsigned int w;
+	unsigned int h;
+
+	//static const unsigned int w;
+	//static const unsigned int h;
+
+	Map( unsigned int _w, unsigned int _h ){
+
+		w = _w;
+		h = _h;
+
+
+
+	    std::cout << "doing a map of size " << _w << "," << _h << std::endl;
+    	unsigned int seed = distribution(generator);
+		PerlinNoise pn(seed);
+
+		for (unsigned int i = 0; i < _w; i++){
 			terrain.push_back( std::vector<Tile>() );
-			for (unsigned int j = 0; j < y; j++){
-				terrain[i].push_back( Tile(i,j,0) );
+			for (unsigned int j = 0; j < _h; j++){
+
+				double x = (double)j/((double)w);
+				double y = (double)i/((double)h);
+				// Typical Perlin noise
+				int k = (int)(255*pn.noise(5 * x*y, 5 * y*x, x*0.8))-127;
+
+				terrain[i].push_back( Tile(i,j,k) );
 			}
 		}
 	    std::cout << "done" << std::endl;
@@ -19,4 +51,5 @@ public:
 	Tile at(int x, int y){
 		return terrain[x][y];
 	};
+
 };
--- src/mapGen/Tile.h
+++ src/mapGen/Tile.h
@@ -1,17 +1,63 @@
+//dunno if we should have all drawing stuff done in one place, but I am doing something here as a test anyway
+#include <SFML/Graphics.hpp>
+int DRAWSIZE = 8;
 class Tile{
 private:
+	enum class Type { DIRT, WATER, SAND };
+    Tile::Type _type;
+	std::vector<int> coords;
+
+public:
+
+	sf::ConvexShape shape;
+
 	static const int x;
 	static const int y;
 	static const int z;
-	std::vector<int> coords;
-public:
+
 	Tile( int x, int y, int z ){
-		x = x;
-		y = y;
-		z = z;
+		if (z < 0){
+			_type = Tile::Type::WATER;
+		}
+		else{
+		    _type = Tile::Type::DIRT;
+		}
+
+	    //TODO do in seperate func later, from here
+		shape.setPointCount(4);
+
+	    shape.setPoint(0, sf::Vector2f(DRAWSIZE*(x  ) ,DRAWSIZE*(y  )));
+		shape.setPoint(1, sf::Vector2f(DRAWSIZE*(x-1)+1, DRAWSIZE*(y  )));
+		shape.setPoint(2, sf::Vector2f(DRAWSIZE*(x-1)+1, DRAWSIZE*(y-1)+1));
+		shape.setPoint(3, sf::Vector2f(DRAWSIZE*(x  ), DRAWSIZE*(y-1)+1));
+
+		switch(_type){
+			case Tile::Type::DIRT:
+				shape.setFillColor (sf::Color(67-z/2,200-z,67-z/2));
+				break;
+			case Tile::Type::WATER:
+				shape.setFillColor (sf::Color(67+z/2,67+z/2,200+z));
+				break;
+			default : //shouldn't go here
+				shape.setFillColor (sf::Color::Red);
+				break;
+		}
+		//to here
 		coords = {x, y, z};
 	};
+	
+	void refreshShape();
+
 	std::vector<int> pos(){
 		return coords;
 	};
+
+	Tile::Type type(){
+		return _type;
+	}
+
+
+
 };
+
+
