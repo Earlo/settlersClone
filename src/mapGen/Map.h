300f377bc401a39857a37e598ed197136d56279a
Earlo <theearlord@gmail.com>
2016-11-22 16:31:36 +0200

    update to generator, ore and woods

--- src/mapGen/Map.h
+++ src/mapGen/Map.h
@@ -1,5 +1,7 @@
 #include <random>
 #include <time.h>
+#include <math.h>
+
 #include "Tile.h"
 
 #include "perlin/PerlinNoise.h"
@@ -19,37 +21,113 @@ public:
 	unsigned int w;
 	unsigned int h;
 
-	//static const unsigned int w;
-	//static const unsigned int h;
-
 	Map( unsigned int _w, unsigned int _h ){
-
-		w = _w;
-		h = _h;
-
-
+		this->w = _w;
+		this->h = _h;
 
 	    std::cout << "doing a map of size " << _w << "," << _h << std::endl;
-    	unsigned int seed = distribution(generator);
-		PerlinNoise pn(seed);
+	    //init three noise functions with different seeds
+		PerlinNoise pn0(distribution(generator));
+		PerlinNoise pn1(distribution(generator));
+		PerlinNoise pn2(distribution(generator));
+		
+		PerlinNoise pnm(distribution(generator));
+		//noise for wood and ore
+		PerlinNoise pnW0(distribution(generator));
+		PerlinNoise pnW1(distribution(generator));
 
+		PerlinNoise pnO(distribution(generator));
+
+		double w0 = 80.0;
+		double w1 = 30.0;
+		double w2 = 10.0;
+		double wm = 40.0;
+
+		double wC = 30.0;
+		double tw = w0+w1+w2+wC;//+wm;
 		for (unsigned int i = 0; i < _w; i++){
 			terrain.push_back( std::vector<Tile>() );
 			for (unsigned int j = 0; j < _h; j++){
-
-				double x = (double)j/((double)w);
-				double y = (double)i/((double)h);
+				double x = (double)j/((double)w) - 0.5;
+				double y = (double)i/((double)h) - 0.5;
 				// Typical Perlin noise
-				int k = (int)(255*pn.noise(5 * x*y, 5 * y*x, x*0.8))-127;
+				double k = 0;
+				// 4 10 30
+				k += (w0*(2*(pn0.noise(6  * x, 6  * y, x*y) - 0.5)));
+				k += (w1*(2*(pn1.noise(12 * x, 12 * y, x*y) - 0.5)));
+				k += (w2*(2*(pn2.noise(36 * x, 36 * y, x*y) - 0.5)));
+				//double mountain = ( pnm.noise(20 * x, 20 * y, x*y) );
+				//k += 2*wm*pow(mountain,3);
+				k += wC*( 1 - ( std::max(0.1,(2*x)*(2*x)) +std::max(0.1,(2*y)*(2*y))));
+				k = std::min((k/tw),1.0)*255;
 
-				terrain[i].push_back( Tile(i,j,k) );
+				terrain[i].push_back( Tile(i,j,(int)k) );
 			}
 		}
-	    std::cout << "done" << std::endl;
 
+		for (unsigned int i = 0; i < _w; i++){
+			for (unsigned int j = 0; j < _h; j++){
+				if (terrain[i][j].z >= 100){
+						terrain[i][j].setType( Tile::Type::ROCK );
+					}
+				if (terrain[i][j].z >= 0){
+				    std::vector<Tile> v = nextTo(i,j);
+					for( unsigned int n = 0; n < v.size(); n++){
+						if( v[n].type() == Tile::Type::WATER ){
+							terrain[i][j].setType( Tile::Type::SAND );
+						}
+					}	
+				}
+			}
+		}
+
+
+		for (unsigned int i = 0; i < _w; i++){
+			for (unsigned int j = 0; j < _h; j++){
+				double x = (double)j/((double)w) - 0.5;
+				double y = (double)i/((double)h) - 0.5;
+				if ( terrain[i][j].type() == Tile::Type::ROCK ){
+					if ( pnO.noise(8  * x, 8  * y, x*y) > 0.4){
+						terrain[i][j].setType( Tile::Type::ORE );
+					}
+				}
+				if ( terrain[i][j].type() == Tile::Type::DIRT ){
+					double val = (pnW0.noise(10  * x, 8  * y, x*y) + pnW1.noise(8  * x, 10  * y, x*y))/2;
+					if ( val  > 0.55){
+						terrain[i][j].setType( Tile::Type::WOODS );
+					}					
+				}
+			}
+		}
+
+	
+		for (unsigned int i = 0; i < _w; i++){
+			for (unsigned int j = 0; j < _h; j++){
+				terrain[i][j].refreshShape();
+			}
+		}
+
+	    std::cout << "done" << std::endl;
 	};
+	
 	Tile at(int x, int y){
 		return terrain[x][y];
+	};
+
+
+
+	std::vector<Tile> nextTo(int x, int y){
+		std::vector<Tile> ret;
+		for ( int a = -1; a <= 1; a++){
+			for ( int b = -1; b <= 1; b++){
+				if ( a != 0 || b != 0){
+					if( ((a+x) > 0) && ((a+x) < (int)w) && ((b+y) > 0) && ((b+y) < (int)h)){
+						ret.push_back(this->at(a+x,b+y));
+					}
+				}
+			}
+		}
+		return ret;
 	};
 
 };
--- src/mapGen/Tile.h
+++ src/mapGen/Tile.h
@@ -1,63 +1,50 @@
 //dunno if we should have all drawing stuff done in one place, but I am doing something here as a test anyway
 #include <SFML/Graphics.hpp>
-int DRAWSIZE = 8;
+
 class Tile{
-private:
-	enum class Type { DIRT, WATER, SAND };
-    Tile::Type _type;
-	std::vector<int> coords;
 
 public:
 
 	sf::ConvexShape shape;
 
-	static const int x;
-	static const int y;
-	static const int z;
+	enum class Type { DIRT, WOODS, WATER, SAND, ROCK, ORE };
 
-	Tile( int x, int y, int z ){
-		if (z < 0){
+	int x;
+	int y;
+	int z;
+
+	Tile( int _x, int _y, int _z ){
+		this->x = _x;
+		this->y = _y;
+		this->z = _z;
+		if (this->z < 0){
 			_type = Tile::Type::WATER;
 		}
 		else{
 		    _type = Tile::Type::DIRT;
 		}
-
-	    //TODO do in seperate func later, from here
-		shape.setPointCount(4);
-
-	    shape.setPoint(0, sf::Vector2f(DRAWSIZE*(x  ) ,DRAWSIZE*(y  )));
-		shape.setPoint(1, sf::Vector2f(DRAWSIZE*(x-1)+1, DRAWSIZE*(y  )));
-		shape.setPoint(2, sf::Vector2f(DRAWSIZE*(x-1)+1, DRAWSIZE*(y-1)+1));
-		shape.setPoint(3, sf::Vector2f(DRAWSIZE*(x  ), DRAWSIZE*(y-1)+1));
-
-		switch(_type){
-			case Tile::Type::DIRT:
-				shape.setFillColor (sf::Color(67-z/2,200-z,67-z/2));
-				break;
-			case Tile::Type::WATER:
-				shape.setFillColor (sf::Color(67+z/2,67+z/2,200+z));
-				break;
-			default : //shouldn't go here
-				shape.setFillColor (sf::Color::Red);
-				break;
-		}
-		//to here
-		coords = {x, y, z};
+		coords = {this->x, this->y, this->z};
 	};
-	
-	void refreshShape();
-
 	std::vector<int> pos(){
 		return coords;
 	};
-
 	Tile::Type type(){
 		return _type;
-	}
+	};
+	void setType( Tile::Type t ){
+		_type = t;
+		//this->z = 200;
+		//std::cout<<"set";							
+		//std::cout<<"set "<<this->x<<","<<this->y<<","<<this->z<<";"<<std::endl;
 
-
+	};
+	void refreshShape();
+private:
+    Tile::Type _type;
+	std::vector<int> coords;
 
 };
+
+
 
 
