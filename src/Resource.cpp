42de47195d7bed1189ed4b7f6719ca45b4060cc9
Miika Kanerva <miika.kanerva@aalto.fi>
2016-11-22 19:32:33 +0200

    Removed VS Project from the repo & Did some Resources class
    Added cpp file for resources..

--- src/Resource.cpp
+++ src/Resource.cpp
@@ -1,5 +1,27 @@
 #include "Resource.h"
 
-Resource::Resource(int x, int y, RType t, unsigned int amount) {
-	
+Resource::Resource(int x, int y, Resource::RType t, unsigned int amount) {
+
+	switch (t) {
+	case Resource::RType::TREE:
+		break;
+	case Resource::RType::IRON:
+		break;
+	case Resource::RType::STONE:
+		break;
+	}
 }
+
+Resource::~Resource() {}
+
+void take_resource() {
+	switch (this->get_resource_type()) {
+	case Resource::RType::TREE:
+		break;
+	case Resource::RType::IRON:
+		break;
+	case Resource::RType::STONE:
+		break;
+	}
+}
+
--- src/Resource.h
+++ src/Resource.h
@@ -8,6 +8,7 @@ public:
 	Resource(int x, int y, Resource::RType t, unsigned int amount) : x_pos(x), y_pos(y), type(t), resource_amount(amount) { }
 	~Resource(){}
 	unsigned int get_resource_amount() { return resource_amount; }
+	Resource::RType get_resource_type() { return type; }
 	void take_resource();
 
 private:
@@ -15,6 +16,4 @@ private:
 	int x_pos;
 	int y_pos;
 	unsigned int resource_amount;
-
-
 };
