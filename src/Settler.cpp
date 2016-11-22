ca1bd22d9da2278ea21937d4dbfb584999619e6c
Ouppa <otto.kangasmaa@gmail.com>
2016-11-22 18:08:18 +0200

    Syntaksi kuntoon

--- src/Settler.cpp
+++ src/Settler.cpp
@@ -1,31 +1,30 @@
-#pragma once
-#include "Settler.h"
+#include "Settler.hpp"
 
-	Settler::Settler() {}
+Settler::Settler() {}
 
-	int Settler::get_x_position const {
-		return x_pos;
-	}
-	int Settler::get_y_position const {
-		return y_pos;
-	}
-	bool Settler::has_weapon() const {
-		return armed;
-	}
-	Item Settler::get_inventory const {
-		return inventory;
-	}
-	void Settler::get_task const{
-		return current_task;
-	}
+int Settler::get_x_position() const {
+    return x_pos;
+}
+int Settler::get_y_position() const {
+    return y_pos;
+}
+bool Settler::has_weapon() const {
+    return armed;
+}
+Item Settler::get_inventory() const {
+    return inventory;
+}
+Task Settler::get_task() const{
+    return current_task;
+}
 
-	void Settler::set_task(const Task t) {
-		current_task = t;
-	}
-	void Settler::occupy(const Building b);
-	void Settler::defend(const Building b);
-	void Settler::attack(const Building b);
-	void Settler::attack(const Settler s);
-	void Settler::gather(const Item i);
-	void build(Building b);
-	void Settler::idle();
+void Settler::set_task(const Task t) {
+    current_task = t;
+}
+void Settler::occupy(const Building b){};
+void Settler::defend(const Building b){};
+void Settler::attack(const Building b){};
+void Settler::attack(const Settler s){};
+void Settler::gather(const Item i){};
+void build(Building b){};
+void Settler::idle(){};
