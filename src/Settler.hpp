d5a6aa9a7458fdf6d8b6c697877423df3a9dcbdf
Lauri Lappalainen <lauri.s.lappalainen@gmail.com>
2016-11-22 19:56:45 +0200

    Settler pseudocode

--- src/Settler.hpp
+++ src/Settler.hpp
@@ -7,27 +7,27 @@
 #include "Task.hpp"
 
 class Settler {
-    
+
 public:
     Settler();
-    
+
     ~Settler();
-    
+
     int get_x_position() const;
     int get_y_position() const;
     bool has_weapon() const;
     Item get_inventory() const;
     Task get_task() const;
-    
+
     void set_task(const Task t);
+    void build(const Building b);
     void occupy(const Building b);
     void defend(const Building b);
     void attack(const Building b);
     void attack(const Settler s);
     void gather(const Item i);
-    void build(const Building b);
     void idle();
-    
+
 private:
     int x_pos;
     int y_pos;
