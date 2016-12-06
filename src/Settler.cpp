e9935ab3e7aa28876e605fc300a447b241b98a8f
Lauri Lappalainen <lauri.s.lappalainen@gmail.com>
2016-12-06 12:17:44 +0200

    Updated interfaces & algorithms

--- src/Settler.cpp
+++ src/Settler.cpp
@@ -23,41 +23,58 @@
 		current_task = t;
 	}
 	void Settler::build(const Building b) {
-		/*
-		list rl = b.needed_resources(); Check required resources
-		list resource_list = rl.filter(_.amount > 0);
-		while (resource_list.size() > 0 && building.completed() != false)
-			list sl = map.building_list(Stockpile);
-			list stockpile_list = //List of stockpiles with required resources
+		int req_wood = b.get_required_wood();
+		int req_stone = b.get_required_stone();
+		int req_iron = b.get_required_iron();
+
+		//TODO: Switch between resources at random
+		while (req_wood > 0 && get_construction_status() == true) {
+			list sl = game.get_stockpile_list();
+			list stockpile_list = sl.filter(_.wood > 0)
 			Building stock = pathfinder.find_nearest(get_x_position(), get_y_position(), stockpile_list);
 			move(stock.get_x_position, stock.get_y_position);
-			//Select required resource r at random from stockpile
-			stock.get_resource(r);
+			stock.take_wood();
 			move(b.get_x_position, b.get_y_position);
-			b.add_resource(r);
-		*/
+			b.add_resource(Wood);
+			req_wood = b.get_required_wood();
+		}
+		while (req_stone > 0 && get_construction_status() == true) {
+			list sl = game.get_stockpile_list();
+			list stockpile_list = sl.filter(_.stone > 0)
+			Building stock = pathfinder.find_nearest(get_x_position(), get_y_position(), stockpile_list);
+			move(stock.get_x_position, stock.get_y_position);
+			stock.take_stone();
+			move(b.get_x_position, b.get_y_position);
+			b.add_resource(Stone);
+			req_stone = b.get_required_stone();
+		}
+		while (req_iron > 0 && get_construction_status() == true) {
+			list sl = game.get_stockpile_list();
+			list stockpile_list = sl.filter(_.iron > 0)
+			Building stock = pathfinder.find_nearest(get_x_position(), get_y_position(), stockpile_list);
+			move(stock.get_x_position, stock.get_y_position);
+			stock.take_iron();
+			move(b.get_x_position, b.get_y_position);
+			b.add_resource(Iron);
+			req_iron = b.get_required_iron();
+		}
 	}
 	void Settler::occupy(const Building b) {
-		/*
 		move(b.get_x_position, b.get_y_position);
 		b.add_inhabitant(this);
-		map.remove_settler(this);
-		*/
+		game.remove_settler(this);
 	}
 	void Settler::defend(const Building b) {
-		/*
 		if (has_weapon() == false) {
 			gather(Weapon);
 		}
 		move(b.get_x_position, b.get_y_position);
 		b.add_defender(this);
-		map.remove_settler(this);
-		*/
+		game.remove_settler(this);
 	}
 	void Settler::gather(const Item i) {
-		/*
 		if (i == Weapon) {
-			list wl = map.building_list(Weaponsmith);
+			list wl = game.get_weaponsmiths();
 			list l = wl.filter(_.weapons > 0);
 			Building wsmith = pathfinder.find_nearest(get_x_position(), get_y_position(), l);
 			move(wsmith.get_x_position, wsmith.get_y_position);
@@ -65,30 +82,39 @@
 	  }
 		Else {
 			while(get_task() == Gather) {
-				list rl = map.resource_list(i);
+				if (i == Tree) {
+					list rl = game.get_trees();
+				}
+				else if (i == Stone) {
+					list rl = game.get_stones();
+				}
+				else if (i == Iron) {
+					list rl = game.get_irons();
+				}
+				else {
+					throw "Unknown resource!";
+				}
 				resource r = pathfinder.find_nearest(get_x_position(), get_y_position(), rl);
 				move(r.get_x_position, r.get_y_position);
 				r.mine();
-				list sl = map.building_list(Stockpile);
+				list sl = game.stockpile_list();
 				Building stock = pathfinder.find_nearest(get_x_position(), get_y_position(), sl);
 				move(stock.get_x_position, stock.get_y_position);
 				b.store(i);
 			}
 		}
-		*/
 	}
 	void Settler::idle() {
-		//current_task = Idle
+		current_task = Idle;
 	}
 
 	void Settler::move(int x, int y) {
-		/*
 		route r = pathfinder.get_route(get_x_position, get_y_position(), x, y);
 		While (get_x_position() != x && get_y_position != y) {
 			Iterator::<Int, Int> nextPos = r.next();
-			x_pos = std::get<0>(nextPos)
-			y_pos = std::get<1>(nextPos)
+			x_pos = std::get<0>(nextPos);
+			y_pos = std::get<1>(nextPos);
 		}
-		*/
+
 	}
 	void Settler::draw();
