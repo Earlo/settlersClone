5dbaf7be3b77410a8976abb27fc559d817dd60fc
Earlo <theearlord@gmail.com>
2016-12-11 14:38:51 +0200

    improved structure

---
+++ src/logic/Player.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "Settler.h"
+#include "Tasḱ.h"
+
+class Player {
+public:
+	std::vector<Settler> settlers;
+	std::vector<Task> tasks;
+private:
+
+};
--- src/logic/Task.h
+++ src/logic/Task.h
@@ -1,7 +1,6 @@
 #pragma once
 
 class Task {
-
 public:
 
 private:
