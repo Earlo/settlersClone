bd3896348ee29e797aef576d6f398c7fb05b6f3c
Miika <miika.kanerva@aalto.fi>
2016-12-14 11:55:55 +0200

    fixed things

---
+++ assets.h
@@ -0,0 +1,52 @@
+#ifndef ASSETS_H
+#define ASSETS_H
+
+#include <SFML/Graphics.hpp>
+class AssetHandler{
+	public:
+	sf::Image TREEIMG;
+	sf::Image STONEIMG;
+	sf::Image IRONIMG;
+	sf::Image FORTRESSIMG;
+	sf::Image SETTLERIMG;
+	sf::Image SETTLERWIMG;	
+
+	sf::Image MENU_BACKGROUND_IMG;
+	sf::Image FHOUSEIMG;
+	sf::Image WEPSMITHIMG;
+	sf::Image CASTLEIMG;
+	sf::Image FHOUSEICON;
+	sf::Image FORTICON;
+	sf::Image WSICON;
+	sf::Image WAREIMG;
+	sf::Image WAREICON;
+	sf::Image CONTROLPANELIMG;
+	sf::Image INFOIMG;
+	sf::Image BUILD;
+
+	sf::Texture TREETEX;
+	sf::Texture STONETEX;
+	sf::Texture IRONTEX;
+	sf::Texture FORTRESSTEX;
+	sf::Texture SETTLERTEX;
+	sf::Texture SETTLERWTEX;
+	sf::Texture MENU_BACKGROUND_TEX;
+	sf::Texture FHOUSETEX;
+	sf::Texture WEPSMITHTEX;
+	sf::Texture CASTLETEX;
+	sf::Texture FHOUSEICONT;
+	sf::Texture FORTICONT;
+	sf::Texture WSICONT;
+	sf::Texture WARETEX;
+	sf::Texture WAREICONT;
+	sf::Texture CONTROLPANELTEX;
+	sf::Texture INFOTEX;
+	sf::Texture BUILDTEX;
+
+	AssetHandler();
+
+};
+
+AssetHandler ASSETHANDLER;
+
+#endif
